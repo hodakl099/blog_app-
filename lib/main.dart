d6f69a68fee4d332774e6b5fbc808869660ff36a
Mahmuud <93213777+hodakl099@users.noreply.github.com>
2024-05-20 19:55:10 +0200

    setting up supabase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:blog_app/core/theme/app_theme.dart';
 import 'package:blog_app/features/auth/presentataion/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+
+  final url = dotenv.env['SUPA_BASE_URL'];
+  final anonKey = dotenv.env['anon_key'];
+
+  final supabase =  await Supabase.initialize(
+    url: url!,
+    anonKey: anonKey!,
+  );
   runApp(const MyApp());
 }
 
@@ -15,10 +27,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Blog App',
-        theme: AppTheme.darkThemeMode,
+      theme: AppTheme.darkThemeMode,
       home: const SigninPage(),
     );
   }
 }
-
-
